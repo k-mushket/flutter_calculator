9bd1001c54cb04eef51720310c34bba59d5658de
k-mushket <k.mushcat@gmail.com>
2024-02-06 00:05:54 +0200

    Restrict the changing of screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_calculator/calculator.dart';
 import 'package:flutter_calculator/models/calculator_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => CalculatorModel(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Calculator(),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => CalculatorModel(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Calculator(),
+        ),
       ),
-    ),
-  );
+    );
+  });
 }
